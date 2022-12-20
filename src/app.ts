1a20f49ea511a3b040084ecb2dcb06e2a924d359
jsuisjuan <juanc.s.delima@gmail.com>
2022-12-20 13:39:24 -0300

    update server

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
-import cors from "cors";
 import express, { Application } from "express";
+import cors from "cors";
 import ip from "ip";
 
 export class App {
     private readonly app: Application;
     private readonly APPLICATION_RUNNING = 'application is running on:';
+    private readonly ROUTE_NOT_FOUND = 'Route does not exist on the server';
 
     constructor(private readonly port: (string | number) = process.env.SERVER_PORT || 3000){
         this.app = express();
@@ -20,7 +21,7 @@ export class App {
     private routes(): void {
         this.app.use('/patients', (req, res) => {});
         this.app.get('/', (req, res) => res.status(200).send({message: 'Server is up'}));
-        this.app.use('/patients', (req, res) => {});
+        this.app.all('*', (req, res) => res.status(404).send({message: this.ROUTE_NOT_FOUND}));
     };
 
     private middleWare(): void {
